9d2b3f7aac7a0b02da65765e330dd17e09e294f5
林强 <linqiang@souche.com>
2018-11-12 16:10:33 +0800

    [modify] 测试

--- src/main/java/com/arry/demo/controller/WxController.java
+++ src/main/java/com/arry/demo/controller/WxController.java
@@ -3,10 +3,15 @@ package com.arry.demo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/wx")
+@RestController
+@RequestMapping("/wx")
 public class WxController {
-    @RequestMapping("/")
-    public String test(){
+    @RequestMapping("")
+    public String test(String signature, String timestamp, String nonce, String echostr){
+        System.out.println(signature);
+        System.out.println(timestamp);
+        System.out.println(nonce);
+        System.out.println(echostr);
         return "Hello";
     }
 }
