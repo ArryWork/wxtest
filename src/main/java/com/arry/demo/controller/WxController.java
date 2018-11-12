0739f57a2f0ecd6deac96d490955d697a41a4ea5
林强 <linqiang@souche.com>
2018-11-12 15:50:44 +0800

    [modify] 添加返回结果

--- src/main/java/com/arry/demo/controller/WxController.java
+++ src/main/java/com/arry/demo/controller/WxController.java
@@ -1,8 +1,12 @@
 package com.arry.demo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/wx")
 public class WxController {
-
+    @RequestMapping("/")
+    public String test(){
+        return "Hello";
+    }
 }
