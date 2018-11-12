0e7132d6347e0b4d21b82dbde05e729de2991ac9
林强 <linqiang@souche.com>
2018-11-12 16:36:53 +0800

    [add] 添加验证逻辑

--- src/main/java/com/arry/demo/controller/WxController.java
+++ src/main/java/com/arry/demo/controller/WxController.java
@@ -1,5 +1,10 @@
 package com.arry.demo.controller;
 
+import com.arry.demo.util.CheckUtil;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class WxController {
     @RequestMapping("")
     public String test(String signature, String timestamp, String nonce, String echostr){
-        System.out.println(signature);
-        System.out.println(timestamp);
-        System.out.println(nonce);
-        System.out.println(echostr);
-        return "Hello";
+        if (CheckUtil.checkSignature(signature,timestamp,nonce)) {
+            return echostr;
+        }
+        return null;
     }
 }
