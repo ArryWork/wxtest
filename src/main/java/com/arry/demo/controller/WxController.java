b8e6a685ac49209247ac4f4f49ba707b61f4a791
林强 <linqiang@souche.com>
2018-11-13 11:09:27 +0800

    [add] 测试 接受xml

--- src/main/java/com/arry/demo/controller/WxController.java
+++ src/main/java/com/arry/demo/controller/WxController.java
@@ -1,21 +1,26 @@
 package com.arry.demo.controller;
 
 import com.arry.demo.util.CheckUtil;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/wx")
 public class WxController {
-    @RequestMapping("")
+    @GetMapping("")
     public String test(String signature, String timestamp, String nonce, String echostr){
         if (CheckUtil.checkSignature(signature,timestamp,nonce)) {
             return echostr;
         }
         return null;
     }
+
+    @PostMapping
+    public String getXml(String xml){
+        System.out.println(xml);
+        return "success";
+    }
+
 }
